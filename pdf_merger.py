ba78988b9eed65176885c3f129123c197c8fae03
Nakshatram Shreyas <62690959+shreyasnax2451@users.noreply.github.com>
2024-02-06 17:49:23 +0530

    Create pdf_merger.py

---
+++ pdf_merger.py
@@ -0,0 +1,13 @@
+from pypdf import PdfWriter
+
+class PDFFiles:
+    def add_pdfs(self, file_paths):
+        merger = PdfWriter()
+        with open('MERGER_OUTPUT.pdf', 'wb') as merger_pdf:
+            for pdf_file in file_paths:
+                merger.append(pdf)
+            merger.write(merger_pdf)
+
+pdffiles = PDFFiles()
+# Can add numerous files. Here, I'm adding two files.
+pdffiles.add_pdfs(['YOUR_FILE_PATH1.pdf','YOUR_FILE_PATH2.pdf'])
